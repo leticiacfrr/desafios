fb831f5f4e928411fa443c3bb8349787c5ea0008
leticiacfrr <leticiakhalily@gmail.com>
2024-08-09 18:27:15 -0300

    novos desafios

--- desafios/desafioaleatorio6.py
+++ desafios/desafioaleatorio6.py
@@ -1,6 +1,6 @@
 print(' \n CONTANDO DÍGITOS! \n ')
 numero = ''
-while numero != 0:
+while numero != 's':
     numero = input('Insira um número inteiro: ')
     print('Este é o número {}'.format(numero))
     print('Esta é a quantidade de dígitos {}'.format(len(numero)))
